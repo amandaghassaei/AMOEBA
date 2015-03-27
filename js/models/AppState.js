e9471b35128e0c2be717602ad2685be10fcb7341
Amanda Ghassaei <amandaghassaei@gmail.com>
2015-03-27 03:10:24 -0400

    small changes

--- js/models/AppState.js
+++ js/models/AppState.js
@@ -243,8 +243,7 @@ AppState = Backbone.Model.extend({
                 this.set("shift", state);
                 break;
             case 68://d delete mode
-                if (this.get("cellMode") == "cell") this.set("deleteMode", state);//only for cell mode
-                else this.set("deleteMode", false);
+                this.set("deleteMode", state);
                 break;
             case 69://e
 //                if (currentTab != "sketch") return;
