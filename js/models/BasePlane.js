53f0200025a04615b7b1f5577a0727d4ab618f8f
Amanda Ghassaei <amandaghassaei@gmail.com>
2015-05-26 23:08:46 -0700

    fixed jitter in tri face highlighter

--- js/models/BasePlane.js
+++ js/models/BasePlane.js
@@ -165,6 +165,7 @@ OctaBasePlane = BasePlane.extend({
     },
 
     calcHighlighterPosition: function(face, position){
+        position.z = 0;
         var index = globals.lattice.getIndexForPosition(position);
         if (index.z%2 != 0) index.x -= 1;
         index.z = this.get("zIndex") - 1;//pretend we're on the top of the cell underneath the baseplane
