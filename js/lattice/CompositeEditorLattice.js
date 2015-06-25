4d57f5553c40a1e8ee540bc3d59beb15d1f59c25
Amanda Ghassaei <amandaghassaei@gmail.com>
2015-06-24 21:50:25 -0700

    references to chid materials

--- js/lattice/CompositeEditorLattice.js
+++ js/lattice/CompositeEditorLattice.js
@@ -79,9 +79,22 @@ define(['underscore', 'backbone', 'appState', 'globals', 'plist', 'three', 'thre
                 sparseCells: JSON.parse(JSON.stringify(this.sparseCells)),
                 cellsMin: cellsMin,
                 cellsMax: cellsMax,
+                elementaryChildren: this._getChildCellTypes(true),
+                compositeChildren: this._getChildCellTypes(false),
                 dimensions: _dimensions
             };
             return data;
+        },
+
+        _getChildCellTypes: function(elementaryTypes){
+            var children = [];
+            this._loopCells(this.sparseCells, function(cell){
+                if (!cell) return;
+                var isComposite = cell.materialName.substr(0,5) == "super";
+                if ((elementaryTypes && !isComposite) || (!elementaryTypes && isComposite)) children.push(cell.materialName);
+            });
+            if (children.length == 0) return null;
+            return _.uniq(children);//remove duplicates
         },
 
 
