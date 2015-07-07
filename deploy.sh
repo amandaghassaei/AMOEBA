7f5ad664b734fc86c0da3acb33e82457b412843b
Amanda Ghassaei <amandaghassaei@gmail.com>
2015-07-07 11:33:14 +0400

    updated file structure

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-rsync -r -v --exclude=".*" --exclude='node/node_modules/' ~/Research/WebApps/DMDesign ~/Projects/dma/Web/mod
+rsync -r -v --exclude=".*" --exclude='node/node_modules/' ~/Research/WebApps/DMDesign/DMDesign ~/Projects/dma/Web/mod
 
 cd ~/Projects/dma
 hg addremove .
