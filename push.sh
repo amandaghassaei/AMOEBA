e6ba3fbdc6b251c9188b3a30083f16d19446ddc0
Amanda Ghassaei <amandaghassaei@gmail.com>
2015-08-26 19:09:08 -0400

    no push.sh

--- push.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-rsync -r -v --exclude=".*" --exclude='node/node_modules/' ~/Research/WebApps/DMDesign/DMDesign ~/Projects/dma/Web/
-
-cd ~/Projects/dma
-hg addremove .
-hg ci -m "deploy from script"
-hg push
