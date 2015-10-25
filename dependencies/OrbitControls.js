58addc91c41f70844e71442e7d13ec3413bef4e1
Amanda Ghassaei <amandaghassaei@gmail.com>
2015-10-24 23:27:29 -0400

    changed orbit controls back

--- dependencies/OrbitControls.js
+++ dependencies/OrbitControls.js
@@ -76,7 +76,7 @@ THREE.OrbitControls = function ( object, domElement ) {
 	this.keys = { LEFT: 37, UP: 38, RIGHT: 39, BOTTOM: 40 };
 
 	// Mouse buttons
-	this.mouseButtons = { ORBIT: THREE.MOUSE.RIGHT, ZOOM: THREE.MOUSE.MIDDLE, PAN: THREE.MOUSE.RIGHT };
+	this.mouseButtons = { ORBIT: THREE.MOUSE.LEFT, ZOOM: THREE.MOUSE.MIDDLE, PAN: THREE.MOUSE.RIGHT };
 
 	////////////
 	// internals
