28e56315f5b0db735f9a4ce7f01d7465c44d516e
amandaghassaei <amandaghassaei@gmail.com>
2017-03-09 20:50:27 -0500

    eod

--- js/classes/Material.js
+++ js/classes/Material.js
@@ -1,3 +1,12 @@
 /**
  * Created by amandaghassaei on 3/9/17.
  */
+
+
+function Material(json){
+
+}
+
+Material.prototype.destroy = function(){
+
+};
--- js/models/materials.js
+++ js/models/materials.js
@@ -5,3 +5,15 @@
 //all stls
 //all textures
 //all materials
+define(["underscore", "materialsPlist", "Material"], function(_, materialPlist, Material){
+
+    var allSTLs = {};
+    var allTextures = {};
+    var allMaterials = {};
+
+    //init default materials
+    _.each(materialPlist.allMaterials, function(materialJSON, id){
+        allMaterials[id] = new Material(materialJSON);
+    });
+
+});
